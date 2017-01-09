522ccfd4ab4777ff7e65c0cdadac0b5a87550965
Leon Do <leondo123@gmail.com>
2017-01-09 10:28:51 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+Part of Speech =
+verb
+noun
+adjective
+adverb
+preposition
+conjunction
+interjection
+
+Colors and Mood =
+yellow: joy
+orange:excitement
+red:intense   
+magenta: sophisticated
+violet: imaginative
+blue: calm
+cyan: calm x 2
+green: restful
+
+Match =
+verb: orange
+noun: green
+adjective: violet
+adverb: yellow
+preposition: blue
+conjunction: cyan
+interjection: red
+determiner: magenta
---
+++ assets/app.js
@@ -0,0 +1,70 @@
+// Solarized Palette
+var solarized_baseDark  = "#002b36";
+var solarized_baseLight  = "#fdf6e3";
+var solarized_baseZero = "#839496";
+var solarized_yellow  = "#b58900";
+var solarized_orange  = "#cb4b16";
+var solarized_red     = "#dc322f";
+var solarized_magenta = "#d33682";
+var solarized_violet  = "#6c71c4";
+var solarized_blue    = "#268bd2";
+var solarized_cyan    = "#2aa198";
+var solarized_green   = "#859900";
+
+
+
+
+
+var word = ["kick", "cat"]
+
+for (var i = 0; i < word.length; i++){
+
+    $.get('http://api.pearson.com/v2/dictionaries/entries?headword=' + word[i]).done(function(response){
+
+        var partOfSpeech = response.results[0].part_of_speech;
+        console.log(partOfSpeech)
+        changeColor(partOfSpeech);
+
+    }) //$.get
+    
+}//forloop
+
+
+
+
+
+
+
+
+function changeColor(partOfSpeech){
+
+    if (partOfSpeech === 'verb'){
+        $(".solarizedTxt").append("<span style=color:" + solarized_orange + ">" + word + "</span>")
+
+    } else if (partOfSpeech === 'noun'){
+        $(".solarizedTxt").append("<span style=color:" + solarized_green + ">" + word + "</span>")
+
+    } else if (partOfSpeech === 'adjective'){
+        $(".solarizedTxt").append("<span style=color:" + solarized_violet + ">" + word + "</span>")
+
+    } else if (partOfSpeech === 'adverb'){
+        $(".solarizedTxt").append("<span style=color:" + solarized_yellow + ">" + word + "</span>")
+
+    } else if (partOfSpeech === 'preposition'){
+        $(".solarizedTxt").append("<span style=color:" + solarized_blue + ">" + word + "</span>")
+
+    } else if (partOfSpeech === 'conjunction' || partOfSpeech === 'conj'){
+        $(".solarizedTxt").append("<span style=color:" + solarized_cyan + ">" + word + "</span>")
+
+    } else if (partOfSpeech === 'interjection' || partOfSpeech === 'interj'){
+        $(".solarizedTxt").append("<span style=color:" + solarized_red + ">" + word + "</span>")
+
+    } else if (partOfSpeech === 'determiner'){
+        $(".solarizedTxt").append("<span style=color:" + solarized_magenta + ">" + word + "</span>")
+
+    } else if (partOfSpeech === undefined){
+        $(".solarizedTxt").append("<span style=color:" + solarized_baseDark + ">" + word + "</span>")
+    }
+
+
+}//changeColor
